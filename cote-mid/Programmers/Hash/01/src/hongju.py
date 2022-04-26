b83f9d5ec6d55d5eb1b619d8e0b4b556d7d4d6f3
YI-Hongju <to_hongju@protonmail.com>
2022-04-26 15:11:17 +0900

    Hash: Retired runner

--- cote-mid/Programmers/Hash/01/src/hongju.py
+++ cote-mid/Programmers/Hash/01/src/hongju.py
@@ -1,9 +1,59 @@
-def solution(participant, completion):
-    my_dict = {}
+# def solution(participant, completion):
+#     answer = ''
 
-    for i in participant:
-        
-        
-    answer = participant[0]
-    print(answer)
+#     my_hash = {}
+
+#     # Add to Hash with comparison
+#     for runner in participant:
+#         if runner in completion:
+#             my_hash[runner] = 1
+#         else: # Not in completion
+#             my_hash[runner] = 0
+
+#     # Mod. answer
+#     for runner in my_hash: # .keys()
+#         if not my_hash[runner]: # == 0 == False
+#             answer = runner
+
+#     return answer
+
+# Same-name runner:
+from cProfile import run
+
+
+def solution(participant, completion):
+    answer = ''
+
+    my_hash = {}
+    # ls_0 = []
+    # ls_1 = []
+
+    # # Add to Hash with comparison
+    # for runner in participant:
+    #     if runner in completion:
+    #         ls_0.append(runner)
+    #     else: # Not in completion
+    #         ls_1.append(runner)
+
+    #     my_hash[0] = ls_0
+    #     my_hash[1] = ls_1
+
+    # # Mod. answer
+    # for runner in my_hash: # .keys()
+    #     if not my_hash[runner]: # == 0 == False
+    #         answer = runner
+
+    # Generate Hash
+    for runner in participant:
+        if my_hash[runner] != 0:
+            my_hash[runner] += 1
+        else:
+            my_hash[runner] += 1
+
+    # Checker
+    for key in my_hash:
+        if key in completion:
+            my_hash[key] -= 1
+
+    answer = my_hash.keys()[0]
     return answer
