8dda1bcff2b11c5bc2c9497e301752701b225539
YI-Hongju <to_hongju@protonmail.com>
2022-04-30 16:03:17 +0900

    hongju: re-init

---
+++ Programmers/Hash/02/src/hongju_hash_02.py
@@ -0,0 +1,8 @@
+def solution(phone_book):
+    my_hash = dict()
+
+            
+
+solution(["119", "97674223", "1195524421"]) # False
+solution(["123","456","789"]) # True
+solution(["12","123","1235","567","88"]) # False
---
+++ Programmers/Hash/03/src/hongju_hash_03.py
@@ -0,0 +1,22 @@
+def solution(clothes):
+    my_hash = {}
+    # item_ls = []
+
+    for set in clothes:
+        my_hash[set[1]]
+        # item_ls
+        # my_hash[key] = clothes[key]
+
+    answer = 0
+    return answer
+
+solution([
+    ["yellowhat", "headgear"], 
+    ["bluesunglasses", "eyewear"], 
+    ["green_turban", "headgear"]
+])
+solution([
+    ["crowmask", "face"], 
+    ["bluesunglasses", "face"], 
+    ["smoky_makeup", "face"]
+])
